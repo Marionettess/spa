bf0fae6d6cca1bc2cc5ba1100c02a3fd110524f9
Marionettess <1055394048@qq.com>
2018-03-08 16:53:51 +0800

    22

--- 01-first-app/rectangle.js
+++ 01-first-app/rectangle.js
@@ -1,17 +1,43 @@
-var width = document.getElementById('rectangle-width');
-var height = document.getElementById('rectangle-height');
-var calc = document.getElementById('rectangle-calc');
-var perimeter = document.getElementById('rectangle-perimeter');
-var area = document.getElementById('rectangle-area');
-var one = document.getElementById('one');
-var two = document.getElementById('two');
+window.onload = function(){
+  //长度输入框自动获得焦点
+  var rectangle__width = document.getElementById('rectangle__width');
+  rectangle__width.focus();
+  //长度输入框失去焦点
+  rectangle__width.onblur = function(){
+    if(rectangle__width.value == ''){
+      document.getElementById('len').innerHTML = '长度不能为空！';
+    }
+    else if(isNaN(rectangle__width.value)){
+      document.getElementById('len').innerHTML = '长度必须是数字！';
+    }
+    else if(rectangle__width.value<0){
+      document.getElementById('len').innerHTML = '长度必须大于零！';
+    }else{
+      document.getElementById('len').innerHTML = '';
+    }
+  }
+  //宽度输入框失去焦点
+  var rectangle__height = document.getElementById('rectangle__height');
+  rectangle__height.onblur = function(){
+   if(rectangle__height.value == ''){
+      document.getElementById('hei').innerHTML = '宽度不能为空！';
+    }
+   else if(isNaN(rectangle__height.value)){
+      document.getElementById('hei').innerHTML = '宽度必须是数字！';
+   }
+   else if(rectangle__height.value<0){
+      document.getElementById('hei').innerHTML = '宽度必须大于零！';
+   }else{
+      document.getElementById('hei').innerHTML = '';
+   }
 
+  }
+};
 
+//点击计算按钮
 function clickFun(){
-  var w = width.value;
-  var h = height.value;
-  perimeter.value = w*2 + h*2;
-  area.value = w*h;
+  var widVal = document.getElementById('rectangle__width').value;
+  var heiVal = document.getElementById('rectangle__height').value;
+  document.getElementById('rectangle__area').value = widVal * heiVal;
+  document.getElementById('rectangle__cir').value = 2*parseFloat(widVal)+parseFloat(heiVal)*2;
 }
-
-
