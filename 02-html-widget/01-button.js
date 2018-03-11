3f46a9f0e908d5b58c90263feeec90ede15576e8
Marionettess <1055394048@qq.com>
2018-03-11 15:55:45 +0800

    5

--- 02-html-widget/01-button.js
+++ 02-html-widget/01-button.js
@@ -1,9 +1,21 @@
-var btn,
+var $btn,
     i=10,
     timer;
 
-window.onload = function(){
-  btn = window.getElementById("btn");
-  btn.value("同意('+i+'s)");
+$(function(){
+  $btn = $('input[type="button"]');
+  $btn.val('同意 (' + i + ' s)');
+  $btn.attr('disabled', 'disabled');
 
-}
+  timer = window.setInterval(function(){
+    $btn.val('同意(' + i-- + 's)');
+    if(i === -1){
+      window.clearInterval(timer);
+      $btn.val('同意');
+      $btn.removeAttr('disabled');
+    }
+  }, 1000);
+  $btn.click(function(){
+    alert('lalala');
+  });
+});
